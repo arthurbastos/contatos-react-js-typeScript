e515d91d22899e9f9a5f66858e87129558edae8e
ALIEN-ARTHUR\arthur <arthurcoelho.27@gmail.com>
2023-01-23 15:37:57 -0300

    Projeto React js Type Srcpit

---
+++ src/contatos/index.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from "react";
+import { ContatosForm } from './Form';
+import { ContatosList } from './List';
+import { useContatosLogic } from './useContatosLogic'
+import {Contato} from "./contatos";
+
+export const Contatos: FC = () =>{
+
+    const {addContato, contato, contatosList, removeContato} = useContatosLogic();
+
+    const handleFormSubmit = (contato: Contato) => {
+        addContato(contato)
+    }
+
+    return(
+        <>
+            <ContatosForm contato={contato}
+                            submeterFormulario={handleFormSubmit}/>
+            <ContatosList list={contatosList} onDelete={removeContato}/>
+        </>
+    )
+}
